df0581422b459f010124e5c06aca9107f85efda4
shaunbhaskar <73226217+shaunbhaskar@users.noreply.github.com>
2022-04-27 19:51:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+video="";
+status="";
+
+function preload()
+{video=createVideo('video.mp4');
+video.hide();}
+
+function setup(){
+canvas=createCanvas(480,380);
+canvas.center();
+}
+
+function draw(){
+image(video,0,0,480,380);
+}
+
+function start() { objectDetector = ml5.objectDetector('cocossd', modalLoaded);
+ document.getElementById("status").innerHTML = "Status : Detecting Objects"; }
+
+function modalLoaded(){
+console.log("modalLoaded");
+status=true;
+video.loop();
+video.speed(1);
+video.volume(0);
+}
